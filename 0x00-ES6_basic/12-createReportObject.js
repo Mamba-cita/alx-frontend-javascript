69dee67c3ea872bc2ca01d329be62ad2ad93d031
Mamba-cita <EDWINSJOHN52@gmail.com>
2024-07-22 17:59:39 +0300

    initial

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,8 +1,9 @@
 export default function createReportObject(employeesList) {
-    return {
-      allEmployees: {
-        ...employeesList,
-      },
-      getNumberOfDepartments: (employeesList) => Object.keys(employeesList).length,
-    };
-  }
+  return {
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments: (employeesList) =>
+      Object.keys(employeesList).length,
+  };
+}
